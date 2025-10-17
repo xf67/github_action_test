0611cde0c2aa59e40dc0d59b55d54ff669ef782c
xf67 <xf6767@gmail.com>
2025-10-17 15:02:06 +0800

    fix sql inject sec problem

--- src/main/java/com/SqlInjectionExample.java
+++ src/main/java/com/SqlInjectionExample.java
@@ -9,77 +9,77 @@ import java.sql.Statement;
 
 public class SqlInjectionExample {
 
-    public void doLogin(String user, String pass) {
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
+    // public void doLogin(String user, String pass) {
+    //     Connection conn = null;
+    //     Statement stmt = null;
+    //     try {
+    //         conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
 
-            // Unsafe SQL query constructed by concatenating strings.
-            String query = "SELECT * FROM users WHERE user = '" + user + "' AND password = '" + pass + "'";
+    //         // Unsafe SQL query constructed by concatenating strings.
+    //         String query = "SELECT * FROM users WHERE user = '" + user + "' AND password = '" + pass + "'";
 
-            stmt = conn.createStatement();
-            stmt.executeQuery(query);
+    //         stmt = conn.createStatement();
+    //         stmt.executeQuery(query);
 
-            // ...
+    //         // ...
 
-        } catch (SQLException se) {
-            // Handle errors for JDBC
-            se.printStackTrace();
-        } finally {
-            // Finally block used to close resources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-            } catch (SQLException se2) {
-            }
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
+    //     } catch (SQLException se) {
+    //         // Handle errors for JDBC
+    //         se.printStackTrace();
+    //     } finally {
+    //         // Finally block used to close resources
+    //         try {
+    //             if (stmt != null) {
+    //                 stmt.close();
+    //             }
+    //         } catch (SQLException se2) {
+    //         }
+    //         try {
+    //             if (conn != null) {
+    //                 conn.close();
+    //             }
+    //         } catch (SQLException se) {
+    //             se.printStackTrace();
+    //         }
+    //     }
+    // }
     
-//    public void doLogin(String user, String pass) {
-//        Connection conn = null;
-//        PreparedStatement pstmt = null;
-//        ResultSet rs = null;
-//        try {
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
-//
-//            // Safe SQL query using prepared statements.
-//            String query = "SELECT * FROM users WHERE user = ? AND password = ?";
-//
-//            pstmt = conn.prepareStatement(query);
-//            pstmt.setString(1, user);
-//            pstmt.setString(2, pass);
-//
-//            rs = pstmt.executeQuery();
-//
-//            // ...
-//
-//        } catch (SQLException se) {
-//            // Handle errors for JDBC
-//            se.printStackTrace();
-//        } finally {
-//            // Finally block used to close resources
-//            try {
-//                if (rs != null) {
-//                    rs.close();
-//                }
-//                if (pstmt != null) {
-//                    pstmt.close();
-//                }
-//                if (conn != null) {
-//                    conn.close();
-//                }
-//            } catch (SQLException se) {
-//                se.printStackTrace();
-//            }
-//        }
-//    }
+   public void doLogin(String user, String pass) {
+       Connection conn = null;
+       PreparedStatement pstmt = null;
+       ResultSet rs = null;
+       try {
+           conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
+
+           // Safe SQL query using prepared statements.
+           String query = "SELECT * FROM users WHERE user = ? AND password = ?";
+
+           pstmt = conn.prepareStatement(query);
+           pstmt.setString(1, user);
+           pstmt.setString(2, pass);
+
+           rs = pstmt.executeQuery();
+
+           // ...
+
+       } catch (SQLException se) {
+           // Handle errors for JDBC
+           se.printStackTrace();
+       } finally {
+           // Finally block used to close resources
+           try {
+               if (rs != null) {
+                   rs.close();
+               }
+               if (pstmt != null) {
+                   pstmt.close();
+               }
+               if (conn != null) {
+                   conn.close();
+               }
+           } catch (SQLException se) {
+               se.printStackTrace();
+           }
+       }
+   }
 }
