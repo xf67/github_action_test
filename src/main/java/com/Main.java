4e29ae9f537c5cbbaf7134adda067d6d82152df6
xf67 <xf6767@gmail.com>
2025-10-17 14:13:02 +0800

    revise this error

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 
 public class Main {
 
-	private static final Logger logger = 
+	private static final Logger logger = Logger.getLogger(Main.class.getName());
 
 	public int add(int a, int b) {
 		try {
